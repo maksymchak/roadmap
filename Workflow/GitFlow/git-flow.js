fe304eb6ed2970705c23726540dcd0166748b352
maksymchak <maksym4ak@gmail.com>
2020-02-18 16:46:37 +0200

    add git flow

---
+++ Workflow/GitFlow/git-flow.js
@@ -0,0 +1,22 @@
+145  rm -rf new-frontend-2019/
+146  git clone https://gitlab.com/sedna.software/Cafferata/vuurwerktoppers/1.0/new-frontend-2019.git
+147  git clone git@gitlab.com:sedna.software/Cafferata/vuurwerktoppers/1.0/new-frontend-2019.git
+148  ls
+149  cd new-frontend-2019/
+150  ls
+151  ls -a
+152  git flow init
+153  git pull
+154  ls
+155  git checkout develop 
+156  git pull
+157  ls
+158  yarn
+159  yarn start
+160  git flow feature start new-page
+161  git status
+162  git status
+163  git branch -a
+164  history
+
+git clone https://gitlab.com/sedna.software/Cafferata/vuurwerktoppers/1.0/new-frontend-2019.git
