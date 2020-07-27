db3a05dd134e673f625b61b9761fa9432fa1ff58
maksymchak <maksym4ak@gmail.com>
2020-07-28 02:52:59 +0300

    graphql list add

--- GraphQL/graphql-webdev.js
+++ GraphQL/graphql-webdev.js
@@ -1,6 +1,9 @@
 GraphQL
 - Introduction
 - GraphQL Schema & Root Query
+- The Resolve Function & Testing Query
+- Different Type & Type Relations
+- GraphQL Lists
 
 /******************************************************************************************/
 
@@ -187,5 +190,49 @@ const MovieType = new GraphQLObjectType({
         return directors.find(director => director.id === parent.id);
       }
     }
+  }),
+});
+
+
+/*=========================== GraphQL Lists ========================================*/
+
+const movies = [
+  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: "5", name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  {
+    id: "7",
+    name: "Lock, Stock and Two Smoking Barrels",
+    genre: "Crime-Comedy",
+    directorId: "4",
+  },
+];
+
+const directors = [
+  { id: "1", name: "Quentin Tarantino", age: 55 },
+  { id: "2", name: "Michael Radford", age: 72 },
+  { id: "3", name: "James McTeigue", age: 51 },
+  { id: "4", name: "Guy Ritchie", age: 50 },
+];
+
+// Для выведения списка елемнтов мы используем GraphQLList.
+// При помощи метода filter отсеиваем все фильмы в зависимости от автора возвращаем
+//  найденнный список елемента.
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter((movie) => movie.directorId === parent.id);
+      },
+    },
   }),
 });
