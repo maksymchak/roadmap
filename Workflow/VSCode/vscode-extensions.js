6324f61a56e2ac89f77ce6d23295b98b40641826
maksymchak <a.maksymchak@sedna.software>
2020-11-16 02:54:50 +0200

    vscode

---
+++ Workflow/VSCode/vscode-extensions.js
@@ -0,0 +1,70 @@
+VSCode
+- VSCode Extensions для JavaScript
+
+/******************************************************************************************/
+
+
+/*================================== VSCode Extensions для JavaScript =======================================================*/
+
+
+Полный курс по VSCode
+// https://www.youtube.com/watch?v=QeUp3CahkQw
+
+Список расширений
+
+Auto Close Tag
+// https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag
+// Помогает закрывать теги
+
+Auto Rename Tag
+// https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag
+// Автоматически переименовывает теги
+
+CSS Peek
+// https://marketplace.visualstudio.com/items?itemName=pranaygp.vscode-css-peek
+// Быстрее находит нужные стили
+
+Bracket Pair Colorizer 2
+// https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer-2
+// Проще ориентироваться в блокахEditorConfig for VS Code
+
+EditorConfig for VS Code
+// https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+// Для дружбы с editorconfig
+
+ESLint
+// https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+// Поддержка ESLint
+
+open in browser
+// https://marketplace.visualstudio.com/items?itemName=techer.open-in-browser
+// Быстро открывает сайт в браузере
+
+Prettier - Code formatter
+// https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+// Форматирует автоматически код в красивый
+
+vscode-icons
+// https://marketplace.visualstudio.com/items?itemName=vscode-icons-team.vscode-icons
+// Красивые иконки для разных типов файлов
+
+JS Parameter Annotations
+// https://marketplace.visualstudio.com/items?itemName=lannonbr.vscode-js-annotations
+// Показывает название параметров для функций
+
+Import Cost
+// https://marketplace.visualstudio.com/items?itemName=wix.vscode-import-cost
+// Показывает весь импортируемых модулей
+
+Live Server
+// https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+// Позволяет быстро запускать локальный сервер для разработки (отлично подойдет для верстки)
+
+GitLens
+// https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens
+// Отличное расширение для работы с Git. Полная информация о том, кто, когда и как
+// изменял репозиторийadvanced-new-file
+
+advanced-new-file
+// https://marketplace.visualstudio.com/items?itemName=patbenatar.advanced-new-file
+// Намного круче позволяет создавать новые файлы с клавиатуры
